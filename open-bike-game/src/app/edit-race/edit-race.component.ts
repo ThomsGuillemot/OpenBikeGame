7161db7f074dc765e817fec4ca9355fe6c6f7437
Thomas Guillemot <thoms.guillemot@gmail.com>
2023-04-25 14:41:58 +0200

    Angular Component Creation

---
+++ open-bike-game/src/app/edit-race/edit-race.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-race',
+  templateUrl: './edit-race.component.html',
+  styleUrls: ['./edit-race.component.sass']
+})
+export class EditRaceComponent {
+
+}
