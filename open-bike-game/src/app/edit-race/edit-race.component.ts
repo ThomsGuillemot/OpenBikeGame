2f8d67e0c741b916fe08322bdd48247068158602
Thomas Guillemot <thoms.guillemot@gmail.com>
2023-04-25 15:29:25 +0200

    - Change the style from .sass to .scss. - Add the basic Race editing UI

--- open-bike-game/src/app/edit-race/edit-race.component.ts
+++ open-bike-game/src/app/edit-race/edit-race.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-edit-race',
   templateUrl: './edit-race.component.html',
-  styleUrls: ['./edit-race.component.sass']
+  styleUrls: ['./edit-race.component.scss']
 })
 export class EditRaceComponent {
 
