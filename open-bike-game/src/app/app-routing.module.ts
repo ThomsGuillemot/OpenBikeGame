48f3f48e35cbf2d9c3fca108fb525bbf60bb6b4d
Guillemot Thomas <thoms.guillemot@gmail.com>
2023-05-15 23:19:06 +0200

    - add the path to the RaceView

--- open-bike-game/src/app/app-routing.module.ts
+++ open-bike-game/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditRaceComponent } from './edit-race/edit-race.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { RaceViewComponent } from './race-view/race-view.component';
 
 const routes: Routes = [
   { path: 'main-menu', component: MainMenuComponent },
   { path: 'edit-race', component: EditRaceComponent },
+  { path: 'race-view', component: RaceViewComponent },
   { path: '', redirectTo: '/main-menu', pathMatch: 'full' },
 ];
 
