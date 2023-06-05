c0de3a85865287ea40c9659108d2aab089281e9a
Guillemot Thomas <thoms.guillemot@gmail.com>
2023-06-05 23:56:14 +0200

    - Correct the barrel file error

--- open-bike-game/src/app/components/index.ts
+++ open-bike-game/src/app/components/index.ts
@@ -1,4 +1,1 @@
-export * from './edit-race/edit-race.component';
 export * from './main-menu/main-menu.component';
-export * from './race-view/race-view.component';
-export * from './race/race.component';
